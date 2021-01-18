e03db834a0e388d834328c1334500182891760c5
Septher <lumingwei_1995@126.com>
2021-01-18 21:12:33 +0800

    add ram parser and hdisk parser

--- data/label_parser.py
+++ data/label_parser.py
@@ -19,7 +19,7 @@ def load_label():
         }
     return raw_label
 
-def screen_label_parser(raw_screens):
+def parse_screen_label(raw_screens):
     pattern = "(\d+(?:\.\d+)?)"
     screen_dic = dict()
     for screen in raw_screens:
@@ -27,60 +27,68 @@ def screen_label_parser(raw_screens):
         screen_dic[screen] = float(size[0])
     return screen_dic
 
-def cpu_label_parser(raw_cpus):
-    freq_pattern = "(\d+(?:\.\d+)?) GHz"
+def parse_cpu_label(raw_cpus):
+    freq_pattern = "(\d+(?:\.\d+)?) ?GHz"
     company_pattern = "(AMD|Intel)"
     gen_pattern = "Core i(\d)"
     cpu_dic = dict()
     for cpu in raw_cpus:
         freq = re.findall(freq_pattern, cpu)
         gen = re.findall(gen_pattern, cpu)
-        company = re.findall(company_pattern, cpu)
-        # print(cpu + "\t" + str(freq) + "\t" + str(gen) + "\t" + str(company))
-        data = {}
-        if len(freq) > 0:
-            data["freq"] = float(freq[0])
+        # company = re.findall(company_pattern, cpu)
+        data = {
+            "freq": float(freq[0])
+        }
         if len(gen) > 0:
             data["gen"] = float(gen[0])
-        if len(company) > 0:
-            data["company"] = company[0]
+        # if len(company) > 0:
+        #     data["company"] = company[0]
         cpu_dic[cpu] = data
     return cpu_dic
 
-def ram_label_parser(raw_rams):
-    sz_pattern = "(\d+) GB"
+def parse_ram_label(raw_rams):
+    sz_pattern = "(\d+) ?GB?"
     gen_pattern = "DDR(\d+)"
+    ram_dict = dict()
     for ram in raw_rams:
         sz = re.findall(sz_pattern, ram)
         gen = re.findall(gen_pattern, ram)
-        print(ram + "\t" + str(sz) + str(gen))
+        ram_dict[ram] = int(sz[0])
+    return ram_dict
+
+def parse_hdisk_label(raw_hdisk):
+    sz_pattern = "(\d+) (G|T)B?"
+    hdisk_dict = dict()
+    for hdisk in raw_hdisk:
+        result = re.findall(sz_pattern, hdisk)
+        hdisk_dict[hdisk] = int(result[0][0]) * (1024 if result[0][1] == "T" else 1)
+    return hdisk_dict
+
+def parse_gcard_label(raw_gcards):
+    for gcard in raw_gcards:
+        print(gcard)
 
     return
 
-def hdisk_label_smooth():
-    return
-
-def gcard_label_smooth():
-    return
-
-def label_parser(raw_label):
+def parse_label(raw_label):
     parsed_label_path = "processed/labels_parsed.json"
     specs, specs_parsed = dict(), dict()
     for name in ["screen", "cpu", "ram", "hdisk", "gcard"]:
         specs[name] = set([raw_label[asin][name] for asin in raw_label])
         # print("%s: %d" % (name, len(specs[name])))
 
-    # specs_parsed["screen"] = screen_label_parser(specs["screen"])
-    # specs_parsed["cpu"] = cpu_label_parser(specs["cpu"])
-    specs_parsed["ram"] = ram_label_parser(specs["ram"])
-    # specs_parsed["screen"] = screen_label_parser(specs["screen"])
-    # specs_parsed["screen"] = screen_label_parser(specs["screen"])
+    # specs_parsed["screen"] = parse_screen_label(specs["screen"])
+    # specs_parsed["cpu"] = parse_cpu_label(specs["cpu"])
+    # specs_parsed["ram"] = parse_ram_label(specs["ram"])
+    # specs_parsed["hdisk"] = parse_hdisk_label(specs["hdisk"])
+    specs_parsed["gcard"] = parse_gcard_label(specs["gcard"])
     # with open(parsed_label_path, "w") as outfile:
     #     json.dump(specs_parsed, outfile)
     #     outfile.close()
 
 
 def label_smooth():
+
     return
 
 def debug(items):
@@ -91,5 +99,5 @@ def debug(items):
 
 if __name__ == '__main__':
     raw_label = load_label()
-    label_parser(raw_label)
+    parse_label(raw_label)
     label_smooth()
