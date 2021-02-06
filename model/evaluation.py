d6397de22f7d0c14b73427956cd8f6aed5c58cad
Septher <lumingwei_1995@126.com>
2021-02-06 21:40:17 +0800

    fix

--- model/evaluation.py
+++ model/evaluation.py
@@ -1,36 +1,43 @@
 from data.dataset_helper import devices_order
 import torch
+import math
+
+K = 5
 
 def evaluate(output_with_label, data_tag):
     result = dict()
     for index, device in enumerate(devices_order):
-        output_zip_label = [(output[index], task_dict[device]) for output, task_dict in output_with_label]
-
-        device_dict = dict()
-        for k in range(1, 6):
-            # device_dict["precision@%d" % k] = precision(output_zip_label, k, data_tag)
-            device_dict["recall@%d" % k] = recall(output_zip_label, device, k, data_tag)
-            # device_dict["nDCG@%d" % k] = nDCG(output_zip_label, k, data_tag)
-        result[device] = device_dict
+        # output_zip_label = [(output[index], label_dict[device]) for output, label_dict in output_with_label]
+        outputs = torch.cat([output[index] for output, _ in output_with_label], dim=1)
+        labels = torch.cat([label_dict[device] for _, label_dict in output_with_label], dim=1)
+        result[device] = calculate(outputs, labels)
+    show_result(result, data_tag)
     return result
 
-def recall(output_zip_label, device, k, data_tag):
-    sample_cnt = 0
-    tp, fp, fn = 0, 0, 0
-    for output, label in output_zip_label:
-        batch_size = output.shape[0]
-        sample_cnt += batch_size
-        for i in range(batch_size):
-            _, top_k_index = torch.topk(output[i], k=k)
-            top_k_set = set(top_k_index.cpu().int().numpy())
-            if label[i].item() in top_k_set:
-                tp += 1
-            else:
-                fn += 1
+log2 = [1.0 / math.log2(k + 2) for k in range(K)]
+def calculate(outputs, labels):
+    sample_cnt = outputs.size()[0]
+    tp, _DCG = [0] * K, [0] * K
+    for i in range(sample_cnt):
+        actual = labels[i].item()
+        _, indices = torch.sort(outputs[i], descending=True)
+        pos = torch.where(indices == actual, indices + 1, torch.zeros_like(indices)).max().item() - 1
+        for k in range(K):
+            if pos <= k:
+                tp[k] += 1
+                _DCG[k] += log2[pos]
+    out = dict()
+    for k in range(K):
+        # recall@k = (number of top_k contains label) / sample_cnt
+        out["recall@%d" % (k + 1)] = 1.0 * tp[k] / sample_cnt
+        # precision@k = (number of top_k contains label) / (sample_cnt * k)
+        out["precision@%d" % (k + 1)] = 1.0 * tp[k] / sample_cnt / k
+        # nDCG: iDCG = 1 because there are only one label for each sample
+        out["nDCG@%d" % (k + 1)] = _DCG[k]
+    return out
 
-    r = 1.0 * tp / (tp + fn)
-    print("%s %s recall@%d: %.4f" % (data_tag, device, k, r))
-    return r
-
-def nDCG(output_zip_label, k, data_tag):
-    return 0
+def show_result(result, data_tag):
+    print("%s training result: " % data_tag)
+    for device in devices_order:
+        for criterion in ["recall"]:
+            print("%s %s: %s" % (device, criterion, str(["%.4f" % result[device]["%s@%d" % (criterion, k + 1)] for k in range(K)])))
